e3052aff8b1f4f6a38647666d4e237699fe254ae
semedsemedov <semedsemedov.div.edu.az>
2025-12-01 20:18:25 +0400

    footer added

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
+import Footer from "./components/Site/Footer";
 import Header from "./components/Site/Header";
 
 function App() {
   return (
     <>
       <Header />
+      <Footer/>
     </>
   );
 }
