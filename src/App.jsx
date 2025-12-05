b3d1a3c8ab94a27ddc8f74130e2fe4bdbbea7278
semedsemedov <semedsemedov.div.edu.az>
2025-12-05 19:08:15 +0400

    Muellimler added

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
+import { Section } from "lucide-react";
 import Footer from "./components/Site/Footer";
 import Header from "./components/Site/Header";
-
+import TeachersSectionUpdated from "./components/Site/TeachersSectionUpdated";
 function App() {
   return (
     <>
       <Header />
+      <TeachersSectionUpdated/>
       <Footer/>
     </>
   );
