d3e84f3ef1f865ccb9813a3f6939c48ca46d0f2f
semedsemedov <semedsemedov.div.edu.az>
2025-12-05 22:50:49 +0400

    PartnerStories and StatsStaories added

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,15 @@ import Footer from "./components/Site/Footer";
 import Header from "./components/Site/Header";
 import TeachersSectionUpdated from "./components/Site/TeachersSectionUpdated";
 import { Slider } from "./components/Site/Slider";
+import StatsSection from "./components/Site/StatsSection";
+import PartnersAndStories from "./components/Site/PartnersStories";
 function App() {
   return (
     <>
       <Header />
       <Slider />
+      <StatsSection />
+      <PartnersAndStories />
       <TeachersSectionUpdated />
       <Footer />
     </>
