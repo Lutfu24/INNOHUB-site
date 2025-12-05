62542e7b0accdbb8cd7e515672a190bcb94a5c0d
Lutfu24 <lutfuhm@code.edu.az>
2025-12-05 19:17:43 +0400

    some changes

--- src/components/Site/Header.jsx
+++ src/components/Site/Header.jsx
@@ -9,11 +9,21 @@ import {
   navigationMenuTriggerStyle,
 } from "@/components/ui/navigation-menu";
 import logo from "../../assets/logo2.png";
+import {
+  Menubar,
+  MenubarContent,
+  MenubarItem,
+  MenubarMenu,
+  MenubarSeparator,
+  MenubarShortcut,
+  MenubarTrigger,
+} from "@/components/ui/menubar";
 
 export default function Header() {
   return (
     <header className="flex items-center justify-around my-5">
       <img src={logo} width={200} alt="" />
+
       <NavigationMenu>
         <NavigationMenuList className="flex-wrap">
           <NavigationMenuItem>
@@ -90,6 +100,34 @@ export default function Header() {
           </NavigationMenuItem>
         </NavigationMenuList>
       </NavigationMenu>
+
+      <div className="flex gap-3">
+        <button className="rounded-full p-2 border border-gray-300">🔍</button>
+        <select
+          name="AZ"
+          className="rounded-full border border-gray-300 py-2"
+          id=""
+        >
+          <option value="AZ">AZ</option>
+          <option value="EN">EN</option>
+          <option value="RU">RU</option>
+        </select>
+        <Menubar>
+          <MenubarMenu>
+            <MenubarTrigger>AZ</MenubarTrigger>
+            <MenubarContent>
+              <MenubarItem className="w-2">AZ</MenubarItem>
+              <MenubarSeparator />
+              <MenubarItem>EN</MenubarItem>
+              <MenubarSeparator />
+              <MenubarItem>RU</MenubarItem>
+            </MenubarContent>
+          </MenubarMenu>
+        </Menubar>
+        <button className="rounded-3xl bg-[#02C8FE] text-white px-5 py-2">
+          Müraciət et
+        </button>
+      </div>
     </header>
   );
 }
