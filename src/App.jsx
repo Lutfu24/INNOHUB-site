8fcc539dab7402f61ab803591dc4fc011f87196a
Lutfu24 <lutfuhm@code.edu.az>
2025-12-08 15:02:55 +0400

    some changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,3 @@
-import Footer from "./components/Site/Footer";
-import Header from "./components/Site/Header";
-<<<<<<< HEAD
 import { Route, Routes } from "react-router-dom";
 import Home from "./components/Site/Home";
 import MainLayout from "./layout/MainLayout/MainLayout";
@@ -17,22 +14,6 @@ function App() {
         <Route index element={<Dashboard />} />
       </Route>
     </Routes>
-=======
-import TeachersSectionUpdated from "./components/Site/TeachersSectionUpdated";
-import { Slider } from "./components/Site/Slider";
-import StatsSection from "./components/Site/StatsSection";
-import PartnersAndStories from "./components/Site/PartnersStories";
-function App() {
-  return (
-    <>
-      <Header />
-      <Slider />
-      <StatsSection />
-      <PartnersAndStories />
-      <TeachersSectionUpdated />
-      <Footer />
-    </>
->>>>>>> d3e84f3ef1f865ccb9813a3f6939c48ca46d0f2f
   );
 }
 
